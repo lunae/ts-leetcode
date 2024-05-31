8e6cd14f213f737ce6409f4c888ad952188672cb
Manny Luna <emmanuel.luna421@gmail.com>
2024-05-31 16:28:50 -0500

    chore: add revised single number solution

--- solutions/single_number.test.ts
+++ solutions/single_number.test.ts
@@ -2,23 +2,38 @@ import { describe, expect, test } from '@jest/globals';
 
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
 
-function singleNumber(nums: number[]): number {
-    const numberMap = nums.reduce((map: Map<number, number>, num: number) => {
-      if (map.has(num)) {
-        map.set(num, map.get(num)! + 1);
-      } else {
-        map.set(num, 1);
-      }
-      return map;
-    }, new Map<number, number>());
+// function singleNumber(nums: number[]): number {
+//     const numberMap = nums.reduce((map: Map<number, number>, num: number) => {
+//       if (map.has(num)) {
+//         map.set(num, map.get(num)! + 1);
+//       } else {
+//         map.set(num, 1);
+//       }
+//       return map;
+//     }, new Map<number, number>());
     
-    let singleNumber = -1;
-    for (const [key, value] of numberMap.entries()) {
-      if (value === 1) {
-        singleNumber = key;
-      }
+//     let singleNumber = -1;
+//     for (const [key, value] of numberMap.entries()) {
+//       if (value === 1) {
+//         singleNumber = key;
+//       }
+//     }
+//     return singleNumber;
+// };
+
+function singleNumber(nums: number[]): number {
+  const numberSet = new Set<number>();
+  
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (numberSet.has(num)) {
+      numberSet.delete(num);
+    } else {
+      numberSet.add(num);
     }
-    return singleNumber;
+  }
+
+  return numberSet.values().next().value;
 };
 
 const cases: [number[], number][] = [
