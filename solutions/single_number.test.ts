59ccee6452bdccdebc6e507c3e110603dd847148
Manny Luna <emmanuel.luna421@gmail.com>
2024-05-31 16:30:06 -0500

    chore: update single number solution

--- solutions/single_number.test.ts
+++ solutions/single_number.test.ts
@@ -2,25 +2,6 @@ import { describe, expect, test } from '@jest/globals';
 
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
 
-// function singleNumber(nums: number[]): number {
-//     const numberMap = nums.reduce((map: Map<number, number>, num: number) => {
-//       if (map.has(num)) {
-//         map.set(num, map.get(num)! + 1);
-//       } else {
-//         map.set(num, 1);
-//       }
-//       return map;
-//     }, new Map<number, number>());
-    
-//     let singleNumber = -1;
-//     for (const [key, value] of numberMap.entries()) {
-//       if (value === 1) {
-//         singleNumber = key;
-//       }
-//     }
-//     return singleNumber;
-// };
-
 function singleNumber(nums: number[]): number {
   const numberSet = new Set<number>();
   
