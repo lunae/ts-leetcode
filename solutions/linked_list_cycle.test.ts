a15408716b924f8f934872d28967d3c8144db7ae
Manny Luna <emmanuel.luna421@gmail.com>
2024-06-07 20:24:31 -0500

    chore: add linked list cycle solution

---
+++ solutions/linked_list_cycle.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from '@jest/globals';
+import { ListNode, arrayToListNode } from './utils';
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/773/
+
+// TODO: arrayToListNode does not provide a way to add cycles in list. Would need to add functionality to add cycles based on pos.
+
+function hasCycle(head: ListNode | null): boolean {
+  const set = new Set<ListNode>();
+  let current = head;
+
+  let hasCycle = false;
+
+  while (current !== null) {
+    if (set.has(current)) {
+      hasCycle = true;
+      break;
+    } else {
+      set.add(current);
+    }
+    current = current.next;
+  }
+
+  return hasCycle;
+};
+
+const cases: [number[], boolean][] = [];
+
+// test.each(cases)('given array %p, should return %p', (headAsArray: number[], expected: boolean) => {
+//   const head = arrayToListNode(headAsArray);
+//   expect(hasCycle(head)).toEqual(expected);
+// });
